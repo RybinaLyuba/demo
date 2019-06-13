4f55933ba61c8807b27d5f918b23a39eaa90fbc2
Lyuba <lyuba.rybina@mail.ru>
2019-06-13 15:14:51 +0500

    update BookRestController

--- src/main/java/com/example/demo/controller/BookRestController.java
+++ src/main/java/com/example/demo/controller/BookRestController.java
@@ -5,13 +5,14 @@ import com.example.demo.model.Book;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 
@@ -26,26 +27,48 @@ public class BookRestController {
     private BookRepository bookRepository;
 
     @PostMapping(produces = "application/json",consumes = "application/json")
-    public ResponseEntity<Book> postBook(@RequestBody Book book, HttpServletRequest request){
+    public ResponseEntity<Book> postBook(@RequestBody Book book){
         book = bookRepository.save(book);
         logger.info("Book with id={} was created!",book.getId());
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(book);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Book> getBook(
-            @PathVariable Long id,
-            HttpServletRequest request){
+            @PathVariable Long id){
         Book book = bookRepository.getOne(id);
-
 
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(book);
     }
 
-    @GetMapping()
-    public Collection<Book> getAll(HttpServletRequest request){
+    @PutMapping(value = "/{id}",produces = "application/json",consumes = "application/json")
+    public  ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book){
+        return bookRepository.findById(id)
+                .map(record->{
+                    record.setName(book.getName());
+                    Book updatedBook = bookRepository.save(record);
+                    logger.info("Book with id={} was updated!",id);
+                    return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(updatedBook);})
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+    @DeleteMapping(value = "/{id}",produces = "application/json",consumes = "application/json")
+    public ResponseEntity<Long> deleteBook(
+            @PathVariable Long id) {
+        return bookRepository.findById(id)
+                .map(record->{
+                    bookRepository.delete(record);
+                    logger.info("Book with id={} was deleted!",id);
+                    return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(id);})
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<Collection<Book>> getAll(){
         List<Book> listBook = bookRepository.findAll();
-        return listBook;
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(listBook);
     }
 
 }
